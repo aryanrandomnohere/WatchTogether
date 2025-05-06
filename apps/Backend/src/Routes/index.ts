6bd7e6172483e5e756f8fb971d0d101e3572b5dd
Aryan Rathore <aryanrathoreop@gmail.com>
2025-05-06 18:41:19 +0530

    Shifted the repo 1 folder up

--- apps/Backend/src/Routes/index.ts
+++ apps/Backend/src/Routes/index.ts
@@ -4,7 +4,7 @@ import UserRouter from "./UserRouter.js";
 import SocialRouter from "./SocialRouter.js";
 import MediaRouter from "./MediaRelation.js";
 import roomRouter from "./roomRouter.js";
-import shareRouter from "./ShareRouter.js";
+//import shareRouter from "./ShareRouter.js";
 const mainRouter = express.Router();
 
 mainRouter.use("/Auth", authRouter);
@@ -12,5 +12,5 @@ mainRouter.use("/user", UserRouter);
 mainRouter.use("/social", SocialRouter);
 mainRouter.use("/media", MediaRouter);
 mainRouter.use("/room", roomRouter);
-mainRouter.use("/screenshare", shareRouter);
+//mainRouter.use("/screenshare", shareRouter);
 export default mainRouter;
